18641543e394033ba4453118aacdffecd072cc1f
佐々木健佑 <sasakikensuke@MBP-N5UME4JR.local>
2018-09-21 17:07:45 +0900

    [add] imasara commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# ref
+
+https://dotinstall.com/lessons/basic_vuejs_v2/43903
---
+++ js/main.js
@@ -0,0 +1,51 @@
+(function() {
+  'use strict';
+
+  // to way data binding (to UI)
+  // データが更新されるとUIが更新される
+
+  var vm = new Vue({
+    el: '#app',
+    data: {
+      newItem: '',
+      todos: [{
+        title: 'task 1',
+        isDone: false
+      }, {
+        title: 'task 2',
+        isDone: false
+      }, {
+        title: 'task 3',
+        isDone: true
+      }]
+    },
+    methods: {
+      addItem: function() {
+        var item = {
+          title: this.newItem,
+          isDone: false
+        };
+        this.todos.push(item);
+        this.newItem = '';
+      },
+      deleteItem: function(index) {
+        if (confirm('are you sure?')){
+          this.todos.splice(index, 1);
+        }
+      },
+      purge: function(index) {
+        if (!confirm('delete finished?')){
+          return;
+        }
+        this.todos = this.remaining;
+      }
+    },
+    computed: {
+      remaining: function() {
+        return this.todos.filter(function(todo) {
+          return !todo.isDone;
+        });
+      }
+    }
+  });
+})();
