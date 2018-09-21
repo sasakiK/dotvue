9ebc2ff14d99445536d37e139ca3e33edc06b94d
佐々木健佑 <sasakikensuke@MBP-N5UME4JR.local>
2018-09-21 17:25:01 +0900

    [add] mounted

--- js/main.js
+++ js/main.js
@@ -8,16 +8,7 @@
     el: '#app',
     data: {
       newItem: '',
-      todos: [{
-        title: 'task 1',
-        isDone: false
-      }, {
-        title: 'task 2',
-        isDone: false
-      }, {
-        title: 'task 3',
-        isDone: true
-      }]
+      todos: []
     },
     watch: {
       todos: {
@@ -26,6 +17,9 @@
         },
         deep: true
       }
+    },
+    mounted: function() {
+      this.todos = JSON.parse(localStorage.getItem('todos')) || [];
     },
     methods: {
       addItem: function() {
